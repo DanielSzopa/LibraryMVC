9d219d2f85e86ce3d8e344d248e44f0fb9f49442
Daniel Szopa <daniel.szopa333@gmail.com>
2021-09-16 19:08:35 +0200

    Add ExampleData and Migration

--- LibraryMVC.Infrastructure/Context.cs
+++ LibraryMVC.Infrastructure/Context.cs
@@ -26,7 +26,7 @@ namespace LibraryMVC.Infrastructure
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            //Default Data
             modelBuilder.Entity<Category>()
                 .HasData(new Category { Id = 1, Name = "Other" });
 
@@ -37,7 +37,44 @@ namespace LibraryMVC.Infrastructure
                 .HasData(new TypeOfBook { Id = 1, Name = "Other" });
 
             modelBuilder.Entity<Author>()
-               .HasData(new Author { Id = 1, FirstName = "none",LastName = "none", Biography = "none"});
+               .HasData(new Author { Id = 1, FirstName = "none", LastName = "none", Biography = "none" });
+
+            //Example Data
+
+            modelBuilder.Entity<Category>()
+               .HasData(
+               new Category { Id = 2, Name = "Book" },
+               new Category { Id = 3, Name = "Newspaper" }
+               );
+
+            modelBuilder.Entity<Publisher>()
+               .HasData(
+               new Publisher { Id = 2, Name = "New Era" },
+               new Publisher { Id = 3, Name = "Opera" }
+               );
+
+            modelBuilder.Entity<TypeOfBook>()
+               .HasData(
+               new TypeOfBook { Id = 2, Name = "Crime Story" },
+               new TypeOfBook { Id = 3, Name = "Romantic" }
+               );
+
+            modelBuilder.Entity<Author>()
+               .HasData(
+               new Author { Id = 2, FirstName = "Katarzyna", LastName = "Bonda", Biography = "Polska dziennikarka i scenarzystka, autorka bestsellerowych powieści kryminalnych" },
+               new Author { Id = 3, FirstName = "Agatha", LastName = "Christie", Biography = "Brytyjska autorka powieści kryminalnych i obyczajowych" }
+               );
+
+            modelBuilder.Entity<Book>()
+                .HasData(
+                new Book { Id = 1, Title = "Lampiony", DateOfRelease = new DateTime(2010, 01, 01), Description = "Sasza Załuska zostaje przyjęta na cywilny etat do gdańskiej policji i natychmiast oddelegowana do zadania w Łodzi.", Status = Status.Active, PublisherId = 2, CategoryId = 2, TypeOfBookId = 2, AuthorId = 2 },
+                new Book { Id = 2, Title = "Lalka", DateOfRelease = new DateTime(2000, 01, 01), Description = "Historia zamożnego kupca warszawskiego Stanisława Wokulskiego i jego miłości do pięknej, lecz zubożałej arystokratki Izabeli Łęckiej.", Status = Status.Active, PublisherId = 3, CategoryId = 2, TypeOfBookId = 2, AuthorId = 3 },
+                new Book { Id = 3, Title = "Pan Tadeusz", DateOfRelease = new DateTime(1990, 01, 01), Description = "Ta epopeja narodowa (z elementami gawędy szlacheckiej) powstała w latach 1832–1834 w Paryżu.", Status = Status.Active, PublisherId = 3, CategoryId = 2, TypeOfBookId = 2, AuthorId = 3 },
+                new Book { Id = 4, Title = "Biografia Daniela Szopy", DateOfRelease = new DateTime(2015, 01, 01), Description = "Czasopismo opowiadające o początku kariery Junior Developera", Status = Status.Active, PublisherId = 2, CategoryId = 3, TypeOfBookId = 2, AuthorId = 2 },
+                new Book { Id = 5, Title = "Wesele", DateOfRelease = new DateTime(2016, 01, 01), Description = "„Wesele” stanowi najważniejszy młodopolski dramat. w Łodzi.", Status = Status.Active, PublisherId = 2, CategoryId = 2, TypeOfBookId = 2, AuthorId = 2 },
+                new Book { Id = 6, Title = "Miłość w Zakopanem", DateOfRelease = new DateTime(2018, 01, 01), Description = "Historia dwojga ludzi, którzy zakochali się w Zakopanem", Status = Status.Active, PublisherId = 2, CategoryId = 2, TypeOfBookId = 2, AuthorId = 3 }
+            );
+
         }    
     }
 }
---
+++ LibraryMVC.WebApplication/Migrations/20210916170546_InitializeExampleData.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryMVC.WebApplication.Migrations
+{
+    public partial class InitializeExampleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Biography", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 2, "Polska dziennikarka i scenarzystka, autorka bestsellerowych powieści kryminalnych", "Katarzyna", "Bonda" },
+                    { 3, "Brytyjska autorka powieści kryminalnych i obyczajowych", "Agatha", "Christie" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 2, "Book" },
+                    { 3, "Newspaper" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 2, "New Era" },
+                    { 3, "Opera" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TypeOfBooks",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 2, "Crime Story" },
+                    { 3, "Romantic" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "CategoryId", "DateOfRelease", "Description", "PublisherId", "Status", "Title", "TypeOfBookId" },
+                values: new object[,]
+                {
+                    { 1, 2, 2, new DateTime(2010, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sasza Załuska zostaje przyjęta na cywilny etat do gdańskiej policji i natychmiast oddelegowana do zadania w Łodzi.", 2, 2, "Lampiony", 2 },
+                    { 2, 3, 2, new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Historia zamożnego kupca warszawskiego Stanisława Wokulskiego i jego miłości do pięknej, lecz zubożałej arystokratki Izabeli Łęckiej.", 3, 2, "Lalka", 2 },
+                    { 3, 3, 2, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ta epopeja narodowa (z elementami gawędy szlacheckiej) powstała w latach 1832–1834 w Paryżu.", 3, 2, "Pan Tadeusz", 2 },
+                    { 4, 2, 3, new DateTime(2015, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Czasopismo opowiadające o początku kariery Junior Developera", 2, 2, "Biografia Daniela Szopy", 2 },
+                    { 5, 2, 2, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "„Wesele” stanowi najważniejszy młodopolski dramat. w Łodzi.", 2, 2, "Wesele", 2 },
+                    { 6, 3, 2, new DateTime(2018, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Historia dwojga ludzi, którzy zakochali się w Zakopanem", 2, 2, "Miłość w Zakopanem", 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TypeOfBooks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TypeOfBooks",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
