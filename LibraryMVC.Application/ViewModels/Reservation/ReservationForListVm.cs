d896a43eb2c4990042f57b02e7224594efdfeb2d
Daniel Szopa <daniel.szopa333@gmail.com>
2021-11-22 18:25:11 +0100

    Create reservation DTOs

---
+++ LibraryMVC.Application/ViewModels/Reservation/ReservationForListVm.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using LibraryMVC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryMVC.Application
+{
+    public class ReservationForListVm : IMapFrom<Reservation>
+    {
+        public int Id { get; set; }
+        public string Customer { get; set; }
+        public string Book { get; set; }
+        public int CustomerId { get; set; }
+        public int BookId { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Reservation, ReservationForListVm>()
+                .ForMember(d => d.Customer, opt => opt.MapFrom(s => s.Customer.FirstName + " " + s.Customer.LastName))
+                .ForMember(d => d.Book, opt => opt.MapFrom(s => s.Book.Title));
+        }
+    }
+}
---
+++ LibraryMVC.Application/ViewModels/Reservation/ReservationListVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryMVC.Application
+{
+    public class ReservationListVm
+    {
+        public List<ReservationForListVm> ListOfReservationForListVm { get; set; }
+        public string SearchString { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+
+    }
+}
