9896fd58a5f9bcb6dfa5927275569b827584acb3
daniel.szopa333 <daniel.szopa333@gmail.com>
2021-12-17 22:02:31 +0100

    Create controller for Roles Management and View for Roles

---
+++ LibraryMVC.WebApplication/Controllers/RoleController.cs
@@ -0,0 +1,26 @@
+﻿using LibraryMVC.Application;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.WebApplication.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private readonly IUserService _userService;
+        public RoleController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            var listOfRoles = _userService.GetAllRolesToList();
+            return View(listOfRoles);
+        }
+    }
+}
